7386ecbc944abf4d0f5319e2497da08aea0ac3ed
Valdir Silva <valdirpiresba@gmail.com>
2022-05-07 00:39:32 -0300

    textarea criado e estilizado com tailwind e tailwincss/forms

---
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,43 @@
+import { feedbackTypes, FeedbackType } from "..";
+import { CloseButton } from "../../CloseButton";
+import { ArrowLeft } from "phosphor-react";
+
+interface FeedbackContentStepProps {
+    feedbackType: FeedbackType;
+    onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackContentStep({ 
+    feedbackType, 
+    onFeedbackRestartRequested
+}: FeedbackContentStepProps) {
+    const feedbackTypeInfo = feedbackTypes[feedbackType]
+
+    return (
+        <>
+        <header>
+            <button type="button" 
+                className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+                onClick={onFeedbackRestartRequested}
+            >
+                <ArrowLeft weight="bold" className="w-4 h-4" />
+            </button>
+            <span className="text-xl leading-6 flex items-center gap-2">
+                <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+                {feedbackTypeInfo.title}
+            </span>
+
+            <CloseButton/>
+       </header>
+
+       <form className="my-4 w-full">
+          <textarea 
+             className="min-w-[304px] w-full min-h-[112px] placeholder-zinc-400 
+             text-sm text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 
+             focus:ring-brand-500 focus:ring-1 resize-none focus:autoline-none scrollbar scrollbar-thumb-zinc-700 
+             scrollbar-track-transoarent scrollbar-thin " 
+             placeholder="Conte com detalhes o que o que está acontecendo"/>
+       </form> 
+      </>
+    )
+}
