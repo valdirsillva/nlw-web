a77659bb88a4040a7108bfb7299dbb9d6b32fe2d
Valdir Silva <valdirpiresba@gmail.com>
2022-05-08 12:24:34 -0300

    mudando nome do diretorio para web

---
+++ web/server/src/routes.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
+import { PrismaFeedbackRepository } from './repositories/prisma/prisma-feedbacks-repository';
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
+
+export const routes = express.Router();
+
+routes.post('/feedbacks', async (req, res) => {
+    const {type, comment, screenshot} = req.body;
+    
+    const prismaFeedbacksRepository = new PrismaFeedbackRepository();
+    const nodemailerMailAdapter = new NodemailerMailAdapter();
+
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository, nodemailerMailAdapter);
+
+    await submitFeedbackUseCase.execute({
+        type,
+        comment,
+        screenshot,
+    })
+
+    return res.status(201).send();
+});
